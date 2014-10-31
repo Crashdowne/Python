806c2c0e9297549d10488f856b8e81142c4ea67b
Bryan <bkesteloo@outlook.com>
2014-10-31 14:43:58 -0700

    Updated GuessIt

--- GuessIt.py
+++ GuessIt.py
@@ -19,6 +19,9 @@ quit = False
 #Creates the variable for the random number
 randint(1,100)
 
+#Initializes a counter
+count = 0 
+
 #Generates the random number
 random_number = randint(1,100)
 
@@ -48,9 +51,9 @@ while quit == False:
 		user_guess = float(user_guess_input)
 	
 		#Checks to see if the number is > 100 and generates an error
-		if user_guess > 100:
+		if user_guess > 100 or user_guess == 0:
 			print "\n>>> Invalid input, please enter a whole number between 1 and 100\n"
-
+			
 		#Checks to see if the user's guess is correct
 		elif user_guess == random_number:
 			print "\n>>> You are correct!!!"
@@ -58,16 +61,84 @@ while quit == False:
 			print ">>> We hope you had fun!"
 			quit = True
 				
-		#Checks to see if the guess is greater than the random number		
+		#Checks to see if the guess is greater than the random number, prints 	
 		elif user_guess > random_number:
 			
-			print "\n>>> Your guess was too high"
-			print ">>> You guessed -->  " + user_guess_input + "\n"
-			print ">>> You can press 'q' at any time to quit <<\n"
+			if user_guess - random_number <= 10:
+				print "\n>>> Almost there!!"
+				print ">>> Your guess was too high"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+				
+			elif user_guess - random_number > 10 and user_guess - random_number <= 15:
+				print "\n>>> Close!"
+				print ">>> Your guess was too high"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+			
+			elif user_guess - random_number > 15 and user_guess - random_number <= 20:
+				print "\n>>> Getting there!"
+				print ">>> Your guess was too high"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+				
+			else:
+				print "\n>>> Your guess was too high"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
 				
 		#Checks to see if the guess is smaller than the random number		
 		else:
 			
-			print "\n>>> Your guess was too low"
-			print ">>> You guessed -->  " + user_guess_input + "\n"
-			print ">>> You can press 'q' at any time to quit <<\n"
+			if random_number - user_guess <= 10:
+				print "\n>>> Almost there!"
+				print ">>> Your guess was too low"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+		
+			elif random_number - user_guess > 10 and random_number - user_guess <= 15:
+				print "\n>>> Close!"
+				print ">> Your guess was too low"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+		
+			elif random_number - user_guess > 15 and random_number - user_guess <= 20:
+			
+				print "\n>>> Getting there!"
+				print ">>> Your guess was too low"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+				
+			else:
+			
+				print "\n>>> Your guess was too low"
+				print ">>> You guessed -->  " + user_guess_input + "\n"
+				print ">>> You can press 'q' at any time to quit <<\n"
+				
+		#Increments the counter
+		count = count + 1
+		print ">>> You have made " + str(count) + " guess(es)\n"
+
+"""
+	Python is different than some of the other programming languages in how the code is complied. In python the code is not compiled at all,
+rather, it uses an interpreter. Interpreters are similar to compilers in the way that they translate high-level languages into machine code.
+But they differ in the way that they translate the code. Interpreters execute the code sequentially, that is, line by line, and will halt when
+an error is found. The code is converted into 'intermediate code', and executed line by line.  Whereas, compilers builds the entire program, 
+then translates it into machine code(1). Other differences between Python and other languages are the code conventions. Python uses indentation,
+handles variables more dynamically, and can be more straight forward that other languages(2). Python does not make use of brackets, rather it uses
+indentation, which is different than other languages. It also does not require the user to define variables explicitly, which allows it to handle
+variables more dynamically. Python also uses some more natural language in its code, such as 'or' rather than '||' (which is used in Java)(2).
+
+	With this information, we can see some pros and cons to the use of Python. The first, and quite possibly the most important, con to using Python
+is that it is slower. This is due to the nature of an interpreter than a compiler. Because an interpreter executes lie by line, it performs slower
+than a compiler, which will execute a chunk of code(2). Python also appears to be a bit more friendly than other languages, such as Java, for 
+beginners. This is because the code feels more like English than other languages. It is also more forgiving than some other languages, in the way
+that variables do not have to be explicitly defined, it can interpret them. Whereas, other languages are more strict. with this in mind, it appears 
+as though Python may be a good starting point for beginners, rather than a more strict programming language.
+
+	Because Marina and I have already taken CSC 110, we found Python to be much more comfortable than Scratch. In some ways Scratch felt more clunky
+and even somewhat confusing. It feels like Scratch is a step backwards, and its simplicity actually makes it more difficult to use. Because of our
+previous experience with Java, we were able to catch onto Python fairly quickly. Albeit it still takes time to really learn a new programming 
+language.
+
+"""
